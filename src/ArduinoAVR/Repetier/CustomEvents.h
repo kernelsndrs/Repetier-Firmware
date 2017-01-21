2892fdf25bf28c4238ae7dd43e251fdf5df5e56e
Kyle P(win) <kylep@1ix.com>
2017-01-21 01:29:44 -0500

    derp, forgot to enable the header method overrides

--- src/ArduinoAVR/Repetier/CustomEvents.h
+++ src/ArduinoAVR/Repetier/CustomEvents.h
@@ -1,8 +1,8 @@
 #ifndef CUSTOM_EVENTS_H_INCLUDED
 #define CUSTOM_EVENTS_H_INCLUDED
 
-//#define EVENT_WAITING_HEATER(id) {WSLED_WaitingHeater(id);}
-//#define EVENT_HEATING_FINISHED(id) {WSLED_HeatingFinished(id);}
+#define EVENT_WAITING_HEATER(id) {WSLED_WaitingHeater(id);}
+#define EVENT_HEATING_FINISHED(id) {WSLED_HeatingFinished(id);}
 #define EVENT_TIMER_100MS {WSLED_Loop();}
 #define EVENT_INITIALIZE {WSLED_Init();}
 void WSLED_Init();
